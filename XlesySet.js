6f63c3e8a6d39121f43919eb0114d08b801b5c54
hikma8272726 <officlgamexc@gmail.com>
2025-03-27 15:10:06 +0700

    Add files via upload

---
+++ XlesySet.js
@@ -0,0 +1,17 @@
+// config.js
+const XlesySet = {
+    owner: ['6285878878149'],
+    packname: 'ʀɪᴢxᴍᴀɪ',
+    author: 'Gxyenn',
+    botname: 'Riz 𝘽𝙤𝙩✓',
+    listprefix: ['.'],
+    listv: ['❀','✿','⚘','々'],
+    number_bot: "", // Kalo pake panel bisa masukin nomor di sini, jika belum ambil session. Format: '628xx'
+    dana: ['kosong'],
+    gopay: ['kosong'],
+    ovo: ['kosong'],
+    pulsa: ['6288980060388'],
+    qris: ['kosong'],
+};
+
+module.exports = XlesySet
